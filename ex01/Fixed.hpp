28f190eb01b2a468d32767f13b9d4298743befcd
akyoshid <akyoshid@student.42tokyo.jp>
2025-07-16 17:46:04 +0000

    [SAVE] done ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 11:11:08 by akyoshid          #+#    #+#             */
+/*   Updated: 2025/07/16 17:37:05 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EX01_FIXED_HPP_
+# define EX01_FIXED_HPP_
+
+# include <iostream>
+
+class Fixed {
+ public:
+    Fixed();
+    explicit Fixed(const int i);
+    explicit Fixed(const float f);
+    Fixed(const Fixed& src);
+    Fixed& operator=(const Fixed& src);
+    ~Fixed();
+    int getRawBits() const;
+    void setRawBits(const int raw);
+    float toFloat() const;
+    int toInt() const;
+ private:
+    int raw;
+    static const int fractional_bits = 8;
+    static const int scaling_factor = 1 << fractional_bits;
+    static const int max_int = (1 << (31 - fractional_bits)) - 1;
+    static const int min_int = -(1 << (31 - fractional_bits));
+    static const double max_flp;
+    static const double min_flp;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& fixed);
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 12:42:04 by akyoshid          #+#    #+#             */
+/*   Updated: 2025/07/16 15:32:02 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main() {
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    return 0;
+}
