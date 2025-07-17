82b3d38f24870d67c34fdecf7fb24446f921de45
akyoshid <akyoshid@student.42tokyo.jp>
2025-07-17 11:31:35 +0000

    [FEAT] Point class

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,29 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:47:21 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/17 10:47:22 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/07/17 11:30:20 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+#include "Fixed.hpp"
+
+Point::Point() : x(0), y(0) {
+}
+
+Point::Point(const float x, const float y) : x(x), y(y) {
+}
+
+Point::Point(const Point& src) : x(src.x), y(src.y) {
+}
+
+Point& Point::operator=(const Point& src) {
+    static_cast<void>(src);
+    std::cerr
+        << "Warning: x and y are const variables, "
+        << "so their values cannot be changed." << std::endl;
+    return (*this);
+}
+
+Point::~Point() {
+}
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,11 +6,25 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:47:35 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/17 10:48:07 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/07/17 10:59:50 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EX03_POINT_HPP_
 # define EX03_POINT_HPP_
+
+# include "Fixed.hpp"
+
+class Point {
+ public:
+    Point();
+    Point(const float x, const float y);
+    Point(const Point& src);
+    Point& operator=(const Point& src);
+    ~Point();
+ private:
+    const Fixed x;
+    const Fixed y;
+};
 
 #endif
